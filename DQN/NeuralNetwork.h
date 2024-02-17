2b1d4b12955bfe78ead4594c435ef7d01f634f0f
Alex <alexrussellneil@gmail.com>
2024-02-17 23:17:36 +0800

    Feed Forward Pass Implemented
    FF Pass implemented with the use of float arrays in the NeuralNetwork Class

--- DQN/NeuralNetwork.h
+++ DQN/NeuralNetwork.h
@@ -9,6 +9,13 @@ class NeuralNetwork
 {
 private:
 	int nnSize = 0;
+	int inputLayerSize;
+	int hiddenLayerNumber;
+	int hiddenLayerSize;
+	int outputLayerSize;
+	float learningRate = 0.01;
+	
+
 	//std::vector <Layer*> layers;
 	float** myNeuralNetworkNodes = nullptr;
 	float** myNeuralNetworkWeights = nullptr;
@@ -18,15 +25,24 @@ private:
 
 	std::random_device rd;
 
+	float* HeNormal_Init(int prevNodeNumber, int currNodesNumber, std::mt19937& generator);
+
+
 public:
-	NeuralNetwork(int inputLayerSize, int hiddenLayerNumber, int hiddenLayerSize, int outputLayerSize, float learningRate);
+	NeuralNetwork(int inputLayerSize, int hiddenLayerNumber, int hiddenLayerSize, int outputLayerSize, float learningRate = 0.01);
+	void print();
 	~NeuralNetwork();
+
+	void SetInputLayer(float* InputLayer);
+
 	
 	bool AddLinearLayer(int prevLayerSize, int currLayerSize, char* weightInitType = new char[9] {'H', 'e', 'N', 'o', 'r', 'm', 'a', 'l', '\0'}); // returns 1 on success, 0 - fail, He Normal Weight Init by default
 	bool AddLeakyReLULayer(int prevLayerSize, int currLayerSize, char* weightInitType = new char[9] {'H', 'e', 'N', 'o', 'r', 'm', 'a', 'l', '\0'}, float koef = 0.01); // same as above
 	bool AddReLULayer(int prevLayerSize, int currLayerSize, char* weightInitType = new char[9] {'H', 'e', 'N', 'o', 'r', 'm', 'a', 'l', '\0'}); // same as above
 
 	bool TransactionFF(); // Feed Forward 
+
+	bool BackPropogation(float* targetOutput);
 
 
 	void setInputLayer(Layer* initLayer);
